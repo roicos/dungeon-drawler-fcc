1a7a9f27cad9bbcde355e024d5f32bb8f2938809
Ekaterina Dontsova <ekaterina.dontsova@gmail.com>
2017-04-09 22:36:47 -0700

    start to dig

--- main.js
+++ main.js
@@ -34,19 +34,124 @@ class Сharacteristics extends React.Component {
 }
 
 class Board extends React.Component {
-  constructor() {
-      super();
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        "board" : this.createBoard(80, 200, 15)
+      }
+  }
+
+  createBoard(width, height, roomsNum){ // 0-wall, 1-dungeon
+    var board = [];
+
+    // create walls
+    for(var i=0; i<height; i++){
+      var row = [];
+      for (var j=0; j<width; j++){
+        row.push(0);
+      }
+      board.push(row);
+    }
+
+    function checkRoom(room){
+      for(var y=room.begin.row-2; y<=room.end.row+2; y++){
+          for(var x=room.begin.col-2; x<=room.end.col+2; x++){
+            check = (board[y] != undefined && board[y][x]);
+            if(!check){
+              return false;
+            }
+          }
+        }
+      return true;
+    }
+
+    function areRoomsCompleted(rooms){
+      for(var i=0; i<rooms.length; i++){
+        if(rooms[i].completed){
+          return false;
+        }
+      }
+      return true;
+    }
+
+    // initiate rooms
+    var rooms = [];
+    for(var num=0; num<roomsNum; num++){
+      var check = false;
+      while(check != true){
+        var row = Math.floor(Math.random() * (height-1));
+        var col = Math.floor(Math.random() * (width-1));
+        var room = {"begin" : {"row" : row-1, "col":col-1}, "end" : {"row" : row+1, "col":col+1}, "completed" : false};
+        check = checkRoom(room);
+      }
+      rooms.push(room);
+      for(var i=room.begin.row; i<=room.end.row; i++){
+        for(var j=room.begin.col; j<room.end.col; j++){
+          board[i][j] = 1;
+        }
+      }
+    }
+
+    // dig rooms
+    while (!areRoomsCompleted){
+      for(var i=0; i<rooms.length; i++){
+        if(!rooms[i].completed && checkRoom(rooms[i])){
+          var direction = Math.floor(Math.random());
+          if(direction){ // vertical
+            for(var j=room.begin.col; j<=room.end.col; j++){
+              board[room.begin.row-1][j] = 1;
+              board[room.end.row+1][j] = 1;
+            }
+          } else { // horizontal
+            for(var j=room.begin.row; j<=room.end.row; j++){
+              board[j][room.begin.col-1] = 1;
+              board[j][room.end.col+1] = 1;
+            }
+          }
+        } else {
+          rooms[i].completed = true;
+        }
+      }
+    }
+
+    // dig corridors
+
+    return board;
   }
 
   render(){
+
+    var trs = [];
+    for(var i = 0; i < this.state.board.length; i++){
+      trs.push(<Tr key={i} index={i} data={this.state.board[i]}/>);
+    }
+
     return(
         <div id="board">
           <div id="dungeon">
             <table>
-              <tbody></tbody>
+              <tbody>{trs}</tbody>
             </table>
           </div>
         </div>
+    );
+  }
+}
+
+class Tr extends React.Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render(){
+    var tds = [];
+    for(var i = 0; i < this.props.data.length; i++){
+      tds.push(<td key={i} className={this.props.data[i] ? "room" : "wall"}></td>);
+    }
+
+    return(
+      <tr>{tds}</tr>
     );
   }
 }
